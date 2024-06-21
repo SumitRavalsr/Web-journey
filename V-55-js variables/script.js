8660a6dedca9d3c7118b6306908d503cda835af6
SumitRaval <sumitraval120@gmail.com>
2024-06-22 01:58:44 +0530

    55

---
+++ V-55-js variables/script.js
@@ -0,0 +1,20 @@
+console.log("Hello ")
+
+var a=5;
+var b=10;
+let c="kkk";
+
+{
+    var a=50;
+    console.log(a)
+
+}
+
+console.log(a)
+
+{
+    let b=100;
+    console.log(b)
+    
+}
+console.log(b)
